d5bc3812b447b29b0504b858848bd8313ddbe8cf
Dan Siroky <dan.siorky@gmail.com>
2021-03-18 22:20:02 -0500

    add twilio notification on startup

--- clock.py
+++ clock.py
@@ -3,9 +3,12 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from vaccine import check_for_appointments
+from notify.twilio import send_message_to_twilio
 
 sched = BlockingScheduler()
 print("starting")
+if "TWILIO_AUTH_TOKEN" in os.environ:
+    send_message_to_twilio('Vaccine Watch server starting up')
 
 # Ideally we would have one process schedule the jobs
 # and another process run the jobs, but this is fine for now.
